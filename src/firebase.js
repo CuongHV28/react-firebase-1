7e680dbf96f184b694a91d2f3a72099cc6ac2e6b
DESKTOP-KLA954L\hoang <tony.phung@sotatek.com>
2022-02-17 20:48:45 +0700

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,69 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCf5uddRsyokpNZ6rd5Lz_QpWKgVq-MByU",
+  authDomain: "serene-star-341613.firebaseapp.com",
+  projectId: "serene-star-341613",
+  storageBucket: "serene-star-341613.appspot.com",
+  messagingSenderId: "539421569050",
+  appId: "1:539421569050:web:4da0d0d57a4df59872a1a3",
+  measurementId: "G-377CNSGKLS",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const googleProvider = new GoogleAuthProvider();
+const signInWithGoogle = async () => {
+    try {
+        const res = await signInWithPopup(auth, googleProvider);
+        const user = res.user;
+        const q = query(collection(db, "users"), where("uid", "==", user.uid));
+        const docs = await getDocs(q);
+        if (docs.docs.length === 0) {
+            await addDoc(collection(db, "users"), {
+                uid: user.uid,
+                name: user.displayName,
+                authProvider: "google",
+                email: user.email
+            });
+        }
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const logInWithEmailAndPassword = async (email, password) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (err){
+        console.log(err);
+        alert(err.message);
+    }
+}
